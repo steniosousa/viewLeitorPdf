2b40b5d6abce5d2dca41532bfdade37ca62ba98c
steniosousa <steniosousaf@gmail.com>
2024-07-18 15:05:32 -0300

    count value total and value notes

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 const App = () => {
   const [selectedFile, setSelectedFile] = useState(null);
   const [loading, setLoading] = useState(false)
-
+  const [contador, setContador] = useState(0)
+  const [valueTotal, setValueTotal] = useState(0)
   const handleFileChange = (e) => {
     setSelectedFile(e.target.files[0]);
   };
 
   const handleSubmit = async (e) => {
+
     e.preventDefault();
     setLoading(!loading)
 
@@ -20,35 +22,43 @@ const App = () => {
 
     const formData = new FormData();
     formData.append('pdf', selectedFile);
-
     try {
       const { data } = await axios.post('https://readerpdfandimage.onrender.com/api/v1/ler-pdf', formData, {
         headers: {
           'Content-Type': 'multipart/form-data'
         }
       });
-      setLoading(false)
 
-      exibirInformacoes(data);
+      setValueTotal(valueTotal + parseFloat(data['Valor da nota'].split(' ')[1]))
+
+      setLoading(false)
+      exibirInformacoes(data, contador + 1);
 
     } catch (error) {
       setLoading(false)
-      console.error('Erro ao enviar PDF:', error);
-      alert('Erro ao enviar PDF. Verifique o console para mais detalhes.');
     }
 
   };
 
-  function exibirInformacoes(informacoes) {
-    const terminalContent = document.getElementById('terminal-content');
-    let formattedText = '';
 
+  function exibirInformacoes(informacoes, count) {
+    setContador(count)
+    const terminalContent = document.getElementById('terminal-content');
+
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    let formattedText = '';
     Object.keys(informacoes).forEach(key => {
       formattedText += `${key}: ${informacoes[key]}\n`;
     });
 
-    terminalContent.textContent = formattedText;
+
+    formattedText = `${count}. ${formattedText}`;
+    card.textContent = formattedText;
+    terminalContent.appendChild(card);
   }
+
 
 
   async function health() {
@@ -75,6 +85,8 @@ const App = () => {
       {loading ? (
         <p>Carregando...</p>
       ) : null}
+      <h3>VALOR TOTAL DAS NOTAS ENVIADAS: {valueTotal}</h3>
+      <h3>QUANTIDADE DE NOTAS ENVIADAS: {contador}</h3>
       <div class="terminal-box">
         <pre id="terminal-content">
         </pre>
